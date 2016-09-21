c6ad9477558b31d31b981e790bced0f03b783e03
Joakim <Joakim@JOCKESDATOR>
2016-09-21 10:50:32 +0200

    shoppingcart logic

--- src/Lesson6/Models/ShoppingCart.cs
+++ src/Lesson6/Models/ShoppingCart.cs
@@ -1,11 +1,125 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace Lesson6.Models
 {
-    public class ShoppingCart
+    public partial class ShoppingCart
     {
+        private WebshopRepository _context;
+
+        public ShoppingCart(WebshopRepository context)
+        {
+
+            _context = context;
+        }
+
+
+        string shoppingCartId { get; set; }
+       
+
+        public static ShoppingCart GetCart(WebshopRepository repo,HttpContext context)
+        {                       
+            var cart = new ShoppingCart(repo);          
+
+            cart.shoppingCartId = cart.GetCartId(context);
+
+            return cart;
+        }
+
+        public string GetCartId(HttpContext context)
+        {
+            string cartid;
+             
+            if (context.Request.Cookies["cartId"] == null)
+            {
+                cartid = Guid.NewGuid().ToString();
+                context.Response.Cookies.Append(
+
+                    "cartId",
+                    cartid,
+                    new Microsoft.AspNetCore.Http.CookieOptions()
+                    {
+                        Path = "/",
+                        HttpOnly = false,
+                        Secure = false,
+                        Expires = DateTime.Now.AddMonths(6)
+
+                    });                
+            }
+            else {
+
+                cartid = context.Request.Cookies["cartId"];
+                cartid = HtmlEncoder.Default.Encode(cartid);
+          
+            }
+            return cartid;
+        }
+
+        public void AddToCart(Product product)
+        {
+
+            var cartItem = _context.Carts.SingleOrDefault(
+                m => m.CartId == shoppingCartId &&
+                m.ProductId == product.ProductId
+                
+                );
+
+            if (cartItem == null)
+            {
+
+                cartItem = new Cart
+                {
+                    ProductId = product.ProductId,
+                    CartId = shoppingCartId,
+                    Count = 1,
+                    DateCreated = DateTime.Now
+                    
+                };
+                _context.Add(cartItem);
+            }
+
+            else
+            {
+                cartItem.Count++;
+                
+            }
+            _context.SaveChanges();
+
+        }
+
+        public int RemoveFromCart (int id)
+        {
+            var cartItem = _context.Carts.SingleOrDefault(
+
+                m => m.CartId == shoppingCartId &&
+                m.ItemId == id
+
+                );
+            int itemcount = 0;
+
+            if (cartItem!=null)
+            {
+                if (cartItem.Count>1)
+                {
+                    cartItem.Count--;
+                    itemcount = cartItem.Count;
+                }
+
+                else
+                {
+                    _context.Carts.Remove(cartItem);
+                    
+                }
+
+                _context.SaveChanges();
+
+            }
+
+            return itemcount;
+        }
     }
 }
